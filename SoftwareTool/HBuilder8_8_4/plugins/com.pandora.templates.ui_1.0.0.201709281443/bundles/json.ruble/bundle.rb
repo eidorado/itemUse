4684eedb2f5607947924599b6cc577ae7d4dd537
home103 <2431471615@qq.com>
2017-10-10 02:08:17 +0800

    HBuilder8_8_4

---
+++ SoftwareTool/HBuilder8_8_4/plugins/com.pandora.templates.ui_1.0.0.201709281443/bundles/json.ruble/bundle.rb
@@ -0,0 +1,18 @@
+require 'ruble'
+
+bundle do |bundle|
+  bundle.author = 'Christopher Williams'
+  bundle.copyright = "Copyright 2010 Aptana Inc. Distributed under the MIT license."
+  bundle.repository = "git://github.com/aptana/json.ruble.git"
+  bundle.display_name = t(:bundle_name)
+  bundle.description = 'JSON bundle ported from James Edward Gray II\'s TextMate bundle. Syntax highlight for <a href="http://json.org/">JavaScript Object Notation</a> files, often used as a lightweight serialization format.'
+  decrease_indent_pattern = /^(.*\*\/)?\s*(\}|\))([^{]*\{)?([;,]?\s*|\.[^{]*|\s*\)[;\s]*)$/
+  increase_indent_pattern = /^.*(\{[^}"'']*|\([^)"'']*|<script(\s+\S*)?>\s*)$/
+  bundle.indent["source.json"] = increase_indent_pattern, decrease_indent_pattern
+    
+  bundle.menu t(:bundle_name) do |main_menu|
+    main_menu.scope = [ 'source.json' ]
+#    main_menu.command '0C3868E4-F96B-4E55-B204-1DCB5A20748B'
+    main_menu.command t(:reformat)
+  end
+end
