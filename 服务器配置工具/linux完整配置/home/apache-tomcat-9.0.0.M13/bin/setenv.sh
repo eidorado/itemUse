34d5ba54429368e1a7a5a058a41f585d2d39e779
FLY <2431471615@qq.com>
2021-06-15 09:36:13 +0800

    1

--- 服务器配置工具/linux完整配置/home/apache-tomcat-9.0.0.M13/bin/setenv.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME=/home/jdk-8u112-linux-x64
-export CATALINA_HOME=/home/apache-tomcat-9.0.0.M13
-export CATALINA_BASE=/home/apache-tomcat-9.0.0.M13
-CATALINA_PID="$CATALINA_BASE/tomcat.pid"
-JAVA_OPTS="-server -Xms512M -Xmx1024M -XX:MaxNewSize=256m -Djava.awt.headless=true"
